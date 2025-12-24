7bc6238d543ad2b88b247d860d4d8ae87074fe35
Anton Yukhnovets <antonyuhnovec@gmail.com>
2025-12-24 17:01:51 +0200

    update base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/developers-portal' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/api-portal' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
